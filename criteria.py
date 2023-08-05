1622893763f6bb0f081c95c7c7f4ee4cffc96ab1
onurdenizs <onurdnz@gmail.com>
2023-08-05 21:51:29 +0300

    criteria screens updated

--- criteria.py
+++ criteria.py
@@ -24,7 +24,7 @@ class Criteria:
         self.text = text #this should be in string
         self.is_numeric = is_numeric # this should be True or False
         self.choices = choices #this is a List of choices
-        self.current_value = current_value #numeric
+        self.current_value = current_value #numeric for numeric criterias or choices id
         Criteria.criteria_db.append(self)
         #print(self.choices)
 
@@ -41,6 +41,43 @@ class Criteria:
         criteria_list.append(Criteria("Arch Support",["Neutral", "Stability"], None))
         criteria_list.append(Criteria("Max Weight", [], None, True))
         criteria_list.append(Criteria("Max Price", [], None, True))
+    def find_criteria_with_id(id):
+        """This function gets an id as integer and returns the corresponding criteria
+        with the given id
+        if no criteria is found it returns None
+        param:
+        id: id to look for (integer)
+        """
+        matching_criteria = None
+        for crit in Criteria.criteria_db:
+            if id == crit.id:
+                matching_criteria = crit
+                break
+        return matching_criteria
+    def change_non_numeric_criteria_current_value_by_index(self, selection_index):
+        """This function gets the id of the selection choice
+        then assigns the current_value of the criteria as the selected value
+        
+        param:
+        selection index: id of the selection choice (integer)"""
+        self.current_value = self.choices[selection_index]
+        
+    
+    def change_numeric_criteria_current_value(self, value):
+        """gets the selection value for a numeric criteria
+        and assigns it as current value of the criteria 
+        as a floating number
+        
+        param:
+        value: Numeric value of the criteria selection """
+        self.current_value = float(value)
+        
+    def remove_criteria_selection(self):
+        """sets the current value of the criteria to None"""
+        self.current_value = None
+
+        
+
                 
 
 
--- main.py
+++ main.py
@@ -44,35 +44,56 @@ def print_header_lower(menu_name):
     print("{0}|{1}|{2}".format(white_spaces,menu_name,white_spaces))
     print(line)
 
-def print_criteria_menu_ingredient_line(criteria):
+def print_criteria_menu_ingredient_line(criteria, modification = False, length = 85):
     """Takes a criteria as an input
     formats and prints it to screen as a menu item
     
     param: 
-    current_menu_choices: possible choices before this function is called (list)
+    modification: If this criteria is choosen for modification or not
     criteria: criteria as criteria class
     """
     menu_line =[]
     menu_item = "[{0}] {1}".format(str(criteria.id),criteria.text.upper())
+    if modification:
+        selection_indicator_head = ">>>"
+        selection_indicator_tail = "<<<"
+        menu_item = "{0}[{1}] {2}{3}".format(selection_indicator_head,str(criteria.id),criteria.text.upper(),selection_indicator_tail)
     menu_item_list = list(menu_item)
-    for i in range(85):
-        menu_line.append("-")
-    menu_line[0] = ":"
-    for i in range(2,len(menu_item_list)+2):
-        menu_line[i] = menu_item_list[i-2]
-    index = len(menu_line)-2
-    for choice in criteria.choices:
-        
-        if criteria.current_value == None or criteria.current_value.lower() != choice.lower():
-            choice_list = list(choice)
-        else:
-            selection_string = "[{0}]".format(choice)
-            choice_list = list(selection_string)
-        for i in range(len(choice_list)-1,-1,-1):
-            menu_line[index] = choice_list[i]
+     
+    if modification == False:
+        for i in range(length):
+            menu_line.append("-")
+        menu_line[0] = ":"
+        for i in range(2,len(menu_item_list)+2):
+            menu_line[i] = menu_item_list[i-2]
+        index = len(menu_line)-2
+        for choice in criteria.choices:
+            if criteria.current_value == None or criteria.current_value.lower() != choice.lower():
+                choice_list = list(choice)
+            else:
+                selection_string = "[{0}]".format(choice)
+                choice_list = list(selection_string)
+            for i in range(len(choice_list)-1,-1,-1):
+                menu_line[index] = choice_list[i]
+                index -= 1
+            menu_line[index]="|"
             index -= 1
-        menu_line[index]="|"
-        index -= 1
+    else:
+        #length = length-(len(selection_indicator_head)+len(selection_indicator_tail))-4*len(criteria.choices)-1
+        for i in range(length):
+            menu_line.append("-")
+        menu_line[0] = ":"
+        for i in range(2,len(menu_item_list)+2):
+            menu_line[i] = menu_item_list[i-2]
+        last_index = len(menu_line)-2
+        choice_list = []
+        for choice_index in range(len(criteria.choices)):
+            corrected_choice_text =  "[{0}] {1} ".format(choice_index, criteria.choices[choice_index])
+            corrected_choice_text_list = list(corrected_choice_text)
+            for letter_index in range(len(corrected_choice_text_list)-1,-1,-1):
+                menu_line[last_index] = corrected_choice_text_list[letter_index]
+                last_index -= 1
+        menu_line[last_index]="|"
         
     menu_line[-1]=":"
 
@@ -140,7 +161,7 @@ def print_main_menu():
     
     
 
-def print_criteria_screen():
+def print_criteria_screen(selectedCriteria = None):
     """
     Prints CRTIERIA MODIFICATION menu to the secreen and ask user for an input
     returns the user input
@@ -149,12 +170,12 @@ def print_criteria_screen():
     current_menu_choices: list of possible menu choices until this function is called
     """
     print_header("MODIFY CRITERIAS")
-    print_current_criteria_selections()
+    print_current_criteria_selections(selectedCriteria)
     
     
      
 
-def print_current_criteria_selections():
+def print_current_criteria_selections(selectedCriteria = None):
     """
     prints current possible choices List to the screen
     and modifies current_menu_choices list by adding current criterias' shortcuts
@@ -165,10 +186,49 @@ def print_current_criteria_selections():
     criteria_menu_choices = []
     print_header_lower("Here are the current Criteria Selections")
     for crit in criteria.Criteria.criteria_db:
-        
-        print_criteria_menu_ingredient_line(crit)
-       
-        
+        if selectedCriteria != None and selectedCriteria == crit:
+            print_criteria_menu_ingredient_line(crit, True)
+        else:
+            print_criteria_menu_ingredient_line(crit)
+            
+
+def print_navigation_menu_screen():
+    print_header("Navigation Menu")
+    print_header("[H] = Return Home [Q] = Quit Program")      
+
+def print_criteria_modification_screen(criteria):
+    os.system('cls')
+    print_header(criteria.text.upper()) 
+    print_criteria_modification_choices(criteria)       
+
+def print_criteria_modification_choices(criteria):
+    """
+    criteria modification screen is printed on the screen
+    first it shows the possible choices (non-numeric criterias)
+    and then shows the current selection and asks for the Update
+    """
+    if criteria.is_numeric == False:
+        print("---------------------------")
+        print("Possible Choices: ")
+        choice_line =""
+        current_selection = "None"
+        if criteria.current_value != None:
+            current_selection = criteria.current_value
+        for choice in criteria.choices:
+            choice_line += " | " + choice
+        print(choice_line)
+        print("---------------------------")
+    
+        print("Current selection is: " + current_selection)
+        print("\n")
+        user_choice = input("Enter the ID of the choice you want to make")
+    else:
+        current_selection = "None"
+        if criteria.current_value != None:
+            current_selection = criteria.current_value
+        print("---------------------------")
+        print("Current value is: " + current_selection)
+
     
 
     
@@ -179,33 +239,105 @@ def print_current_criteria_selections():
 
 
 #MAIN PROGRAM FUNCTIONS STARTS HERE
-def main_menu_screen(current_menu_choices):
+def main_menu_screen(current_menu_choices=[]):
     """
     All the operations related to main/opening screen is in this function
     """
     print_main_menu()
     update_possible_choices(current_menu_choices, main_menu_shortcuts())
-     
+    user_choice = get_user_choice("Please Select an option from the Main Menu items: ")
+    while user_choice.lower() not in current_menu_choices:
+        os.system('cls')
+        current_menu_choices = []
+        print_welcome_screen()
+        main_menu_screen(current_menu_choices)
+        print("You have made an INVALID selection")
+        user_choice = get_user_choice("Please Select an option from the Main Menu items: ")
+
+
+    if user_choice.lower() == "m":
+        os.system('cls')
+        criteria_menu_screen()
+    if user_choice.lower() == "f":
+        pass                                 
+    if user_choice.lower() == "q":
+        print("BYE, hope to see you again!")
+        return    
+def program_navigation_menu_screen(current_menu_choices):
+    "This screen will be printed all the menus so user turn back to main main or quit etc."
+    navigation_shortcuts = ["h", "q"]
+    update_possible_choices(current_menu_choices, navigation_shortcuts)
+    print_navigation_menu_screen()
+
 def criteria_menu_screen():
     """
     All the operations related to criteria menu screen is here
     """
     current_menu_choices = []
     print_criteria_screen()
-    print_main_menu()
-    update_possible_choices(current_menu_choices, main_menu_shortcuts())
+    program_navigation_menu_screen(current_menu_choices)
+        
     update_possible_choices(current_menu_choices,criteria_screen_shortcuts())
     user_choice = get_user_choice("Please Enter The Shortcut of the item you are interested: ")
     while check_user_choice_validity(user_choice,current_menu_choices) == False:
         os.system('cls')
         user_choice = criteria_menu_screen()    
-    return user_choice     
+    if  user_choice.lower() == "h":
+        os.system('cls')
+        current_menu_choices = []
+        print_welcome_screen()
+        main_menu_screen(current_menu_choices)
+        return
+    elif  user_choice.lower() == "q":
+        print("BYE, hope to see you again!")
+        return 
+    else:
+        os.system('cls')
+        current_criteria = criteria.Criteria.find_criteria_with_id(int(user_choice))
+        
+        print_criteria_screen(current_criteria)
+        print("\n\n")
+        
+        if current_criteria.is_numeric:  
+           user_choice = get_user_choice("Please Enter {0}: ".format(current_criteria.text.upper())) 
+           input_Float = is_user_input_float(user_choice)
+           while input_Float == False:
+               os.system('cls')
+               
+               print_criteria_screen(current_criteria)
+               print("!!! This is an INVALID VALUE!!!")
+               print("\n\n")
+               user_choice = get_user_choice("Please Enter {0}: ".format(current_criteria.text.upper())) 
+               input_Float = is_user_input_float(user_choice)
+           user_choice = float(user_choice)
+           #below the selected value of this criteria is assigned 
+           criteria.Criteria.change_numeric_criteria_current_value(current_criteria,user_choice)
+        else:
+            current_menu_choices = []
+            for index in range(len(current_criteria.choices)):
+                current_menu_choices.append(str(index).lower())
+            user_choice = get_user_choice("Please Select The Choice to Apply: ")
+            while check_user_choice_validity(user_choice,current_menu_choices) == False:
+                os.system('cls')
+                print_criteria_screen(current_criteria)
+                print("!!! You have made an INVALID selection!!!")
+                print("\n\n")
+                user_choice = get_user_choice("Please Select The Choice to Apply: ") 
+            #below the selected value of this criteria is assigned    
+            criteria.Criteria.change_non_numeric_criteria_current_value_by_index(current_criteria,int(user_choice) )
+    os.system('cls')
+    criteria_menu_screen()
+    return
 def criteria_screen_shortcuts():
     """"returns a list wich contains list of id's of criterias"""
     criteria_choices= []
     for crit in criteria.Criteria.criteria_db:
         criteria_choices.append(str(crit.id))
     return criteria_choices
+
+def criteria_modification_screen(criteria):
+    print_criteria_modification_screen(criteria)
+
 def main_menu_shortcuts():
     """
     Returns the list of main menu shortcuts
@@ -231,12 +363,20 @@ def get_user_choice(message):
     returns it
     param:
     message: message which will be shown to user while asking for input
-    current_menu_choices: vurrent possible menu slection shortcuts
-    print_funct(): function that calls this get_user_function
+    
     """
     user_choice = input(message)
     
     return user_choice
+
+def is_user_input_float(user_choice):
+    
+    try:
+        float(user_choice)
+        return True
+    except ValueError:
+        return False
+
 def check_user_choice_validity(user_choice,current_menu_choices):
     if user_choice.lower() in current_menu_choices:
         return True
@@ -260,24 +400,7 @@ def run_program():
     initialize_program()
     print_welcome_screen()
     main_menu_screen(current_menu_choices)
-    user_choice = get_user_choice("Please Select an option from the Main Menu items: ")
-    while user_choice.lower() not in current_menu_choices:
-        os.system('cls')
-        current_menu_choices = []
-        print_welcome_screen()
-        main_menu_screen(current_menu_choices)
-        print("You have made an INVALID selection")
-        user_choice = get_user_choice("Please Select an option from the Main Menu items: ")
-
-
-    if user_choice.lower() == "m":
-        os.system('cls')
-        criteria_menu_screen()
-    if user_choice.lower() == "f":
-        pass                                 
-    if user_choice.lower() == "q":
-        print("BYE, hope to see you again!")
-        return    
+        
     
     
 
